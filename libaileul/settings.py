142c04e08ef64db6e01b0af2255d2672fc674bdd
plectre <22507994+Plectre@users.noreply.github.com>
2022-09-14 11:29:19 +0200

    addStatifiles

--- libaileul/settings.py
+++ libaileul/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles/'
-STATICFILES_DIRS = [BASE_DIR / "hangar/static",]
+STATICFILES_DIRS = [BASE_DIR / "hangar/static/",]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
